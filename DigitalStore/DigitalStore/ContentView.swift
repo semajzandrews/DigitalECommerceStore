a354815e9372985b530c66653408438e98ef0147
Semaj Andrews <semajzandrews@gmail.com>
2022-03-26 15:18:10 -0400

    Adding SwiftUI Project

---
+++ DigitalStore/DigitalStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DigitalStore
+//
+//  Created by Semaj Andrews on 3/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DigitalStore/DigitalStore/DigitalStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalStoreApp.swift
+//  DigitalStore
+//
+//  Created by Semaj Andrews on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct DigitalStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
